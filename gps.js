524ad12a0e8be60cadd6c5e22ae04ca0acd9cc8b
Alfa147x <Alfa147x@gmail.com>
2015-04-14 09:26:06 -0400

    Update gps.js

--- gps.js
+++ gps.js
@@ -1,21 +1,23 @@
 var server = require ('tk102');
 
-
 // start server
 server.createServer ({
   port: 1337
 });
 
+//This doesn't work
+server.on ('track', function (gps) {
+	console.log ('GPS: ' + gps.signal);
+});
 
+
+//This works
 server.on ('data', function (raw) {
   console.log ('Incoming data: '+ raw);
 });
 
-
+//This works
 server.on ('connection', function (socket) {
   console.log ('Connection from '+ socket.remoteAddress);
 });
 
-server.on ('track', function (gps) {
-	console.log ('GPS: ' + gps.signal);
-});
